d486f95f9c65cdfb79f5cf74bc9b5a0ab6f227a8
antoniolins <antoniolins@uol.com.br>
2020-10-16 22:28:35 -0300

    Had been implementation Login / Logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('register', views.register, name='register')
-
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout')
 ]
